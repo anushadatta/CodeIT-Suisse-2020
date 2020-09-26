1f58fcede7fbcda453463859eafb4657bc97bdc6
anushadatta <anushadatta@gmail.com>
2020-09-26 16:30:44 +0800

    update

--- codeitsuisse/routes/clean.py
+++ codeitsuisse/routes/clean.py
@@ -20,57 +20,63 @@ def clean():
         moves_count = 0
 
         # Counting moves for dirty floor
-        for i in range(len(dirty_floor)):
+        for i in range(len(dirty_floor)-1):
             
             # Current spot
             current_spot = dirty_floor[i]
             
             # Next spot
-            if i < (len(dirty_floor)-1):
-                next_spot = dirty_floor[i+1]
-            else:
-                moves_count += current_spot
-                break
+            next_spot = dirty_floor[i+1]
             
-            # Count
+            # CASE 1
             if current_spot == 0:
                 continue 
+            
+            # CASE 2
             # eg. 5, 5
             if current_spot == next_spot:
                 moves_count += current_spot + next_spot
                 if (i+1) == (len(dirty_floor)-1): # Last index, whole floor is now clean
                     break
                 else:
+                    moves_count += 1
                     dirty_floor[i+1] = 1
+
+            # CASE 3
             # eg. 1,3
             if current_spot < next_spot:
                 difference = abs(current_spot-next_spot)
                 if(difference%2==0): # Even
-                    moves_count += max(current_spot, next_spot) + 1
+                    moves_count += max(current_spot, next_spot) 
                     if (i+1) == (len(dirty_floor)-1): # Last index, whole floor is now clean
                         break
                     else:
+                        moves_count += 1
                         dirty_floor[i+1] = 1
                 else: # Odd
-                    moves_count += ((current_spot + next_spot)*2) + 1
+                    moves_count += ((current_spot + next_spot)*2) 
                     if (i+1) == (len(dirty_floor)-1): # Last index, whole floor is now clean
                         break
                     else:
+                        moves_count += 1
                         dirty_floor[i+1] = 1
-                
+            
+            # CASE 4
             if current_spot > next_spot:
                 difference = abs(current_spot-next_spot)
                 if(difference%2==0): # Even
-                    moves_count += (max(current_spot, next_spot)*2) + 1
+                    moves_count += (max(current_spot, next_spot)*2) 
                     if (i+1) == (len(dirty_floor)-1): # Last index, whole floor is now clean
                         break
                     else:
+                        moves_count += 1
                         dirty_floor[i+1] = 1
                 else: # Odd
-                    moves_count += (max(current_spot, next_spot)*2) + 1
+                    moves_count += (max(current_spot, next_spot)*2) 
                     if (i+1) == (len(dirty_floor)-1): # Last index, whole floor is now clean
                         break
                     else:
+                        moves_count += 1
                         dirty_floor[i+1] = 0
 
         moves_answer[key] = moves_count
