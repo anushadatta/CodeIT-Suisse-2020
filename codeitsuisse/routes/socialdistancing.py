a0114f50161a2b0a686e0535eacefd863f5b7a45
anushadatta <anushadatta@gmail.com>
2020-09-26 21:55:25 +0800

    update

--- codeitsuisse/routes/socialdistancing.py
+++ codeitsuisse/routes/socialdistancing.py
@@ -9,23 +9,26 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
+def choose(n, r):
+    return int(math.factorial(n) / (math.factorial(r) * math.factorial(n-r)))
+
 @app.route('/social-distancing', methods=['POST'])
 def socialdistancing():
     data = request.get_json()
-    print(data)
+    # print(data)
     tests_dict = data["tests"]
-    print(tests_dict)
+    # print(tests_dict)
     combinations= {}
-
-    def choose(n, r):
-        return int(math.factorial(n) / (math.factorial(r) * math.factorial(n-r)))
 
     for key,dict_data in tests_dict.items():
         seats = dict_data["seats"]
         people = dict_data["people"]
         spaces = dict_data["spaces"]
 
-        number_of_ways = choose(seats - (people-1)*spaces, people)
+        # number_of_ways = choose(seats - (people-1)*spaces, people)
+        a = seats - spaces * (people-1) - people
+        b = people+1
+        number_of_ways = choose(a+b-1,a)
 
         combinations[key] = number_of_ways
 
